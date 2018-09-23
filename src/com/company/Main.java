65c227f92b45f4384e4e38be42dac7c574da93e7
Sanyabelarus <00unclevasya00@mail.ru>
2018-09-23 21:35:00 +0300

    Новое решение

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,21 @@
 package com.company;
 
-import java.util.Random;
-import java.util.Scanner;
+        import java.util.ArrayList;
+        import java.util.Random;
+        import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Student sanya = new Student(scoreIn(),"Александр");
-        Student sveta = new Student(scoreIn(),"Светлана");
-        Student nikita = new Student(scoreIn(),"Никита");
-        Student kirill = new Student(scoreIn(),"Кирилл");
-        Student aleksey = new Student(scoreIn(),"Алексей");
-        Student viktor = new Student(scoreIn(), "Виктор");
-        Student maria = new Student(scoreIn(), "Мария");
-        Student mihail = new Student(scoreIn(), "Михаил");
-        Student katya = new Student(scoreIn(), "Екатерина");
-        Student marina = new Student(scoreIn(), "Марина");
-        Student [] allStudents = {sanya,sveta,nikita,kirill,aleksey,viktor,maria,mihail,katya,marina};
-        enlisted(allStudents);
-
+        String [] names = {"Александр", "Светлана", "Никита", "Кирилл", "Алексей", "Виктор", "Мария", "Михаил", "Екатерина", "Марина"};
+        ArrayList<Student> students = new ArrayList<Student>();
+        students = newStudents(names);
+        enlisted(students);
+        result(students);
 
     }
 
-    public static int[] scoreIn(){
+    public static int[] scoreIn(){                                  // Задание случайных чисел оценок каждому обьекту
         int[] score = new int[3];
         Random random = new Random();
         for(int i = 0; i<3; i++) {
@@ -31,7 +24,7 @@ public class Main {
         return score;
     }
 
-    public static void enlisted(Student[] students) {
+    public static void enlisted(ArrayList<Student> students) {      // Проверка прохождения учеников
         Scanner passingScore = new Scanner(System.in);
         System.out.println("Введите проходной балл от 1 до 10");
         boolean isInt = passingScore.hasNextInt();
@@ -40,14 +33,14 @@ public class Main {
             if(score <= 10 && score >= 0){
                 Student newStudent = new Student();
                 int totalScore = 0;
-                for(int i=0;i<students.length;i++){
-                   newStudent = students[i];
-                   for (int x=0;x<3;x++){
-                       totalScore +=newStudent.score[x];
-                   }
-                   totalScore /=3;
+                for(int i=0;i<students.size();i++){
+                    newStudent = students.get(i);
+                    for (int x=0;x<3;x++){
+                        totalScore +=newStudent.score[x];
+                    }
+                    totalScore /=3;
                     if(totalScore >= score){
-                        System.out.println(students[i].getName() + " зачислен на курс");
+                        System.out.println(students.get(i).getName() + " зачислен на курс");
                     }
                     totalScore = 0;
                 }
@@ -56,6 +49,38 @@ public class Main {
             }
         }else{
             System.out.println("Введите правильное число");
+        }
+    }
+
+    public static ArrayList<Student> newStudents(String[] names){  // Создание студентов
+        ArrayList<Student> students = new ArrayList<Student>();
+        for (int x=0; x<names.length;x++) {
+            Student student = new Student(scoreIn(),names[x]);
+            students.add(student);
+        }
+        return students;
+    }
+
+    public static void result (ArrayList<Student> students) {       //  Поиск ученика и ывод оценок
+        System.out.println("Если вы хотите узнать ваши результаты введите имя");
+        Scanner name = new Scanner(System.in);
+        String trueName = name.nextLine();
+        boolean find = false;
+        for(int x=0;x<students.size();x++){
+            if(trueName.toLowerCase().equals(students.get(x).name.toLowerCase())){
+                System.out.print(students.get(x).getName() + " Оценки по зачислению такие: " );
+               score(students.get(x).score);
+               find = true;
+            }
+        }
+        if (!find){
+            System.out.println("Вы ввели не существующего студента");
+        }
+
+    }
+    public static void score(int [] score) {                    // Вывод каждой оценки
+        for (int i=0;i<score.length;i++) {
+            System.out.print(" " + score[i]);
         }
     }
 }
